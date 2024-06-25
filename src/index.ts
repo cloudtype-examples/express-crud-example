91b6c64b329f8c4e5890607c92a8e9b0d3d5508f
KYEONGMIN CHO <ggingmin@gmail.com>
2024-06-25 16:25:47 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+import express from 'express';
+import bodyParser from 'body-parser';
+import userRoutes from './routes/userRoutes';
+import { AppDataSource } from './data-source';
+
+const app = express();
+const PORT = 3001;
+
+app.use(bodyParser.json());
+
+AppDataSource.initialize().then(() => {
+  app.use('/api/users', userRoutes);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}).catch(error => console.log(error));
